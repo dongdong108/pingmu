894ee8d6e536b7e7456776309daa3708b23e28b3
dongdong108 <liudongdong108@163.com>
2016-12-27 22:11:28 +0800

    限定符

---
+++ PMSPDemo/app/src/main/java/com/result/pmspdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.result.pmspdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * autour: 刘东东
+ * date: 2016/12/27 19:18
+ * update: 2016/12/27
+ * 限定符
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
